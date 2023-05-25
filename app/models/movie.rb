4a623b0f7554b029c0e4f800e705b046feabd009
Adeline de Gaulejac <adelinedegaulejac@gmail.com>
2023-05-25 14:11:31 +0200

    seed faker movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,8 +5,6 @@ class Movie < ApplicationRecord
   validates :title, uniqueness: true, presence: true, allow_blank: false
   validates :overview, uniqueness: true, presence: true, allow_blank: false
 
-  private
-
   # def check_for_presence_in_list
   #   if !@movie.bookmarks.empty?
   #     errors.add_to_base("cannot delete movie while movie is in a list")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Deleting all movie records'
+Movie.delete_all
+puts Movie.count
+
+puts 'Creating 100 fake movies...'
+i = 0
+100.times do
+  movie = Movie.new(
+    title: "#{i} - #{Faker::Movie.title}",
+    overview: "#{Faker::Fantasy::Tolkien.poem} #{i}",
+    poster_url: Faker::LoremFlickr.image
+  )
+  i += 1
+  movie.save!
+end
+puts 'Finished!'
